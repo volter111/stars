1083257aacd320f5b7d66a1a8d8a4ce503140890
volter111 <krut.valeriy@gmail.com>
2021-11-10 14:58:31 -0800

    static added, styles.css, images

--- petproject/petproject/settings.py
+++ petproject/petproject/settings.py
@@ -128,3 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = []
