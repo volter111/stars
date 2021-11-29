7cb8d7857c1483e55a02174b931d83547afe2f0a
volter111 <krut.valeriy@gmail.com>
2021-11-29 12:37:52 -0800

    views refactor, class views added

--- petproject/stars/urls.py
+++ petproject/stars/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from stars.views import *
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', HomePage.as_view(), name='home'),
     path('about/', about, name='about'),
-    path('add_page/', add_page, name='add_page'),
+    path('add_page/', AddPage.as_view(), name='add_page'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
-    path('post/<slug:post_slug>', show_post, name='show_post_page'),
-    path('category/<int:cat_id>', show_category, name='category'),
+    path('post/<slug:post_slug>', ShowPost.as_view(), name='show_post_page'),
+    path('category/<slug:cat_slug>', ShowCategory.as_view(), name='category'),
 ]
 
 handler404 = pageNotFound
--- petproject/stars/views.py
+++ petproject/stars/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView
 from .models import *
 from .forms import *
 
@@ -12,31 +13,57 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
         ]
 
 
-def index(request):
-    posts = Celebrities.objects.all()  # получаем все записи с бд, модель Celebrity
+class HomePage(ListView):
+    model = Celebrities
+    template_name = 'stars/index.html'
+    context_object_name = 'posts'
 
-    context = {
-        'menu': menu,
-        'title': 'Домашнаяя страница',
-        'posts': posts,
-        'cat_selected': 0,
-    }
-    return render(request, 'stars/index.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
+        return context
+
+    def get_queryset(self):
+        return Celebrities.objects.filter(is_published=True)
+
+
+# def index(request):
+#     posts = Celebrities.objects.all()  # получаем все записи с бд, модель Celebrity
+#
+#     context = {
+#         'menu': menu,
+#         'title': 'Домашнаяя страница',
+#         'posts': posts,
+#         'cat_selected': 0,
+#     }
+#     return render(request, 'stars/index.html', context=context)
 
 
 def about(request):
     return render(request, 'stars/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
-def add_page(request):
-    if request.method == 'POST':
-        form = AddPostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = AddPostForm()
-    return render(request, 'stars/add_page.html', {'form': form, 'menu': menu, 'title': 'Добавить запись'})
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'stars/add_page.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Добавить запись'
+        return context
+
+# def add_page(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AddPostForm()
+#     return render(request, 'stars/add_page.html', {'form': form, 'menu': menu, 'title': 'Добавить запись'})
 
 
 def login(request):
@@ -51,29 +78,58 @@ def pageNotFound(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
 
 
-def show_post(request, post_slug):
-    post = get_object_or_404(Celebrities, slug=post_slug)
+class ShowPost(DetailView):
+    model = Celebrities
+    template_name = 'stars/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
 
-    context = {
-        'post': post,
-        'menu': menu,
-        'title': post.title,
-        'cat_selected': post.cat_id,
-    }
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['post']
+        context['menu'] = menu
+        return context
 
-    return render(request, 'stars/post.html', context=context)
+# def show_post(request, post_slug):
+#     post = get_object_or_404(Celebrities, slug=post_slug)
+#
+#     context = {
+#         'post': post,
+#         'menu': menu,
+#         'title': post.title,
+#         'cat_selected': post.cat_id,
+#     }
+#
+#     return render(request, 'stars/post.html', context=context)
 
 
-def show_category(request, cat_id):
-    posts = Celebrities.objects.filter(cat_id=cat_id)
+class ShowCategory(ListView):
+    model = Celebrities
+    template_name = 'stars/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
 
-    if len(posts) == 0:
-        raise Http404()
+    def get_queryset(self):
+        return Celebrities.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
 
-    context = {
-        'menu': menu,
-        'title': 'Отображение по рубрикам',
-        'posts': posts,
-        'cat_selected': cat_id,
-    }
-    return render(request, 'stars/index.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
+
+
+# def show_category(request, cat_id):
+#     posts = Celebrities.objects.filter(cat_id=cat_id)
+#
+#     if len(posts) == 0:
+#         raise Http404()
+#
+#     context = {
+#         'menu': menu,
+#         'title': 'Отображение по рубрикам',
+#         'posts': posts,
+#         'cat_selected': cat_id,
+#     }
+#     return render(request, 'stars/index.html', context=context)
